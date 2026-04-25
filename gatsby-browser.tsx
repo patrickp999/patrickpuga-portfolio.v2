2efca24500809d8408ebadc6c6d49cc432db57bf
Patrick <ppuga999@gmail.com>
2026-04-25 15:36:05 -0600

    Revert "feat(analytics): track client-side navigation with umami"
    This reverts commit 71eb36c09109b73c9718c5e8b5cf6f5c06fee846.

--- gatsby-browser.tsx
+++
@@ -1,5 +0,0 @@
-export const onRouteUpdate = ({ location }: { location: Location }) => {
-  if (typeof window !== "undefined" && (window as any).umami) {
-    (window as any).umami.track();
-  }
-};
